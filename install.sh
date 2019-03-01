ba2bcfa758a401110f7b8efdac9e7f37aefd1b99
Timothée Cercueil <warlkiry@gmail.com>
2019-03-01 09:34:05 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "[*] Installation de natified_scripts en cours..."
+
+echo "[-] Creation du dossier ./.local/natified_apps/"
+mkdir ~/.local/natified_apps
+echo "[-] Extracting scripts"
+unzip natified_scripts.zip ~/.local/natified_apps
